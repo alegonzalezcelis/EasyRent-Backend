3de639685114ce9007b567c83862742c65e14e97
alegonzalezcelis <alegonzalez1993@gmail.com>
2020-09-27 13:55:01 -0300

    Initial commit

---
+++ Migrations/20200925080302_Initialcreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Oracle.EntityFrameworkCore.Metadata;
+
+namespace contactos.Migrations
+{
+    public partial class Initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true),
+                    cedula = table.Column<string>(nullable: true),
+                    mensaje = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacto");
+        }
+    }
+}
